f742b784bf14b9bb73ca7d46d8a0bc28e9d1ef7b
keschaefer <keschaefer@gmail.com>
2018-12-19 11:38:55 -0700

    added add-question component, styled, added router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { QuestionsComponent } from './questions/questions.component';
 import { QuestionSpecificComponent } from './question-specific/question-specific.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { AddQuestionComponent } from './add-question/add-question.component';
 
 const routes: Routes = [
     { path: "", component: QuestionsComponent },
     { path: "login", component: LoginComponent },
     { path: "specific", component: QuestionSpecificComponent },
     { path: "signup", component: SignUpComponent },
+    { path: "add", component: AddQuestionComponent}
 ];
 
 @NgModule({
