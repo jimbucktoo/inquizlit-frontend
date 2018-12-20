089dabea3e8281c904a26a545cf25a74c9cb4cd4
keschaefer <keschaefer@gmail.com>
2018-12-20 14:41:33 -0700

    added ngForm module to answer field in add question component, added answer post request on submit in add question component

--- src/app/add-question/add-question.component.ts
+++ src/app/add-question/add-question.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NewQuestion } from '../new-question';
 import { AddQuestionService } from '../add-question.service';
+import { AddAnswerService } from '../add-answer.service';
+import { NewAnswer } from '../new-answer';
 
 @Component({
     selector: 'app-add-question',
@@ -12,19 +14,22 @@ export class AddQuestionComponent implements OnInit {
 
     tags = ['Culture Fit', 'Algorithms', 'Computer Science', 'Riddles'];
 
-    model = new NewQuestion()
+    model = new NewQuestion();
+
+    modelAnswer = new NewAnswer("", 3, 1, 0, 0);
 
     // submitted = false;
 
     onSubmit() {
     }
 
-    constructor(private service: AddQuestionService) { }
+    constructor(private qsrv: AddQuestionService, private asrv: AddAnswerService) { }
 
     ngOnInit() {
     }
 
     newQuestion() {
-        this.service.postQuestion(this.model);
+        this.qsrv.postQuestion(this.model);
+        this.asrv.postAnswer(this.modelAnswer);
     }
 }
