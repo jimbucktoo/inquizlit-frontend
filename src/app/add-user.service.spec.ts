a8d4e13c6844d9a1a6483f1c871e0d2fd13aea33
keschaefer <keschaefer@gmail.com>
2018-12-21 15:32:48 -0700

    added image previews and correct frontend deployed link

--- src/app/add-user.service.spec.ts
+++ src/app/add-user.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AddUserService } from './add-user.service';
+import { AddNewUserService } from './add-user.service';
 
 describe('AddUserService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: AddUserService = TestBed.get(AddUserService);
+    const service: AddNewUserService = TestBed.get(AddNewUserService);
     expect(service).toBeTruthy();
   });
 });
