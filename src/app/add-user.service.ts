f538e49afbe8ada4573524b9be55ad7727f56f2d
grantfogle <grantjfogle@gmail.com>
2019-01-16 15:40:20 -0700

    login request is working

--- src/app/add-user.service.ts
+++ src/app/add-user.service.ts
@@ -1,33 +1,35 @@
 import { Injectable } from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
 })
 export class AddNewUserService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    postUser(newUser: any){
-      fetch("https://inquizlit-backend.herokuapp.com/users", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json; charset=utf-8"
-        },
-        body: JSON.stringify(newUser)
-      })
+  postUser(newUser: any) {
+    fetch("https://inquizlit-backend.herokuapp.com/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify(newUser)
+    })
       .then(response => (response.json()))
-    };
-
-    sendUser(User: any){
-      fetch("https://inquizlit-backend.herokuapp.com/users", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json; charset=utf-8"
-        },
-        body: JSON.stringify(User)
-      })
-      .then(response => (response.json()))
-    };
-
   };
+
+  sendUser(user: any) {
+    return (
+      fetch("http://localhost:3001/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        },
+        body: JSON.stringify(user)
+      })
+        .then(response => (response.json()))
+    )
+  };
+
+};
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
 import { SigninUser } from "../signin-user";
 import { AddNewUserService } from "../add-user.service";
 
@@ -11,13 +11,28 @@ import { AddNewUserService } from "../add-user.service";
 
 export class LoginComponent implements OnInit {
 
+  question: any;
   model = new SigninUser("", "");
+  message: any;
+
 
   constructor(private router: Router, private usrv: AddNewUserService) { }
 
   onSubmit() {
+    console.log("CATS!!!")
+    this.usrv.sendUser(this.model).then(response => {
+      console.log(response)
+      if (response === "User not found") {
+        this.message = response;
+      } else {
+        this.sendToHome();
+      }
+      //"email": test@test.com
+      //password
+      //"password": $2a$10$WEum/MKIDZPls7oVC7G/8e7HuIa4o.6RM6hAOXIKWWM2Fy5KF94ny
+    })
   };
-  
+
   ngOnInit() {
   };
 
