105bdf0d91de9e3b45cac6f859a9313a9f534151
jimbucktoo <jamesliang.g@gmail.com>
2019-02-04 19:58:47 -0700

    initialization

--- server.js
+++ server.js
@@ -9,4 +9,17 @@ app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/inquizlit-frontend/index.html'));
 });
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
+
 app.listen(process.env.PORT || 8080);
