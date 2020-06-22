f9e7dfe4999786442d1fc92c4f2a7114e9c53063
jimbucktoo <jamesliang.g@gmail.com>
2020-06-22 14:48:18 -0500

    critical correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -32,7 +33,7 @@ import { FooterComponent } from './footer/footer.component'
         FormsModule,
         HttpClientModule,
     ],
-    providers: [],
+    providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
